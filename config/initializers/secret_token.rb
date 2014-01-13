9cb71de8bcc2a152e724cb236ff884bb36fb478e
Daniel Weller <ravensfan55222@aol.com>
2014-01-13 09:32:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pattins::Application.config.secret_key_base = '6b2c5ae00a34db54e4a3c1ebbb90840fdd7eb61c5f09c9262a87ea163ab6508aa12e83ae255186515df05b1acedd0e77b8a46a425b1ebcb0184184483b1ace09'
